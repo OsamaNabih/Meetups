60a5a2406ce213f64d41a0711846e771aaf0e067
Osama Nabih <Nabih_The_4th@yahoo.com>
2018-04-14 18:47:19 +0200

    Refactored local strategy to async/await, attempted to add facebook strategy, commented it out for now, added fields to JOI schema

--- helpers/routeHelpers.js
+++ helpers/routeHelpers.js
@@ -20,13 +20,14 @@ module.exports = {
   },
 
   schemas: {
-    authSchema: Joi.object().keys({
+    signupAuthSchema: Joi.object().keys({
       email: Joi.string().email().required(),
-      password: Joi.string().required().min(7).max(30),
+      authField: Joi.string().required().min(7).max(30),
+      authType: Joi.number().integer().positive().min(1).max(3),
       firstName: Joi.string().required(),
       lastName: Joi.string().required(),
       position: Joi.string(),
-      birthDate: Joi.date().min('1-1-1910').max(`${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()}`)
+      birthDate: Joi.date().required().min('1-1-1910').max(`${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()}`)
     })
   }
 }
--- passport.js
+++ passport.js
@@ -2,11 +2,13 @@ const passport = require('passport');
 const JwtStrategy = require('passport-jwt').Strategy;
 const { ExtractJwt } = require('passport-jwt');
 const LocalStrategy = require('passport-local').Strategy;
+const FacebookTokenStrategy = require('passport-facebook-token');
 const { JWT_SECRET } = require('./config/keys');
 const UserModel = require('./models/user');
-const DB = require('./config/DB');
+const Database = require('./config/DB');
+const DBconfig = require('./config/keys').DBconfig;
 const bcrypt = require('bcryptjs');
-
+const config = require('./config/keys');
 // JSON WEB TOKENS STRATEGY
 /*
 passport.use(new JwtStrategy({
@@ -33,31 +35,57 @@ passport.use(new JwtStrategy({
 passport.use(new LocalStrategy({
   usernameField: 'email',
 
-},  (email, password, done) =>{
+}, async (email, password, done) =>{
   try {
     // Find the user given the email
-          DB.query(UserModel.GetUser(),email,(error,result)=>{
-          if(error)
-            console.log(error);
-          else if (!result) {
-              return done(null, false);
-          }
-          else{
-            // Check if the password is correct
-              const dbPassword = result[0].password;
-                bcrypt.compare(password,dbPassword,function(err,res){    // await should be written here as 3amo
-                 if (!res) {
-                   console.log(res);
-                   return done(null, false);
-                    }
-               // Otherwise, return the user
-                 console.log(res);
-                 done(null, result[0].userId);
-
-              });
-          }
-        });
-    } catch(error) {
-          done(error, false);
+    const DB = new Database(DBconfig);
+    let user = await DB.query(UserModel.GetUser(),email);
+    if(!user){
+      return done(null, false);
     }
+    let authType = user[0].authType;
+    if (authType !== 1){
+      throw 'Email already exists';
+    }
+    const dbPassword = user[0].authField;
+    let isMatch = await bcrypt.compare(password,dbPassword);
+    if (!isMatch){
+      return done(null, false);
+    }
+    done(null, user);
+     // Otherwise, return the user
+    done(null, user[0].userId);
+  } catch(error){
+    done(error, false);
+  }
 }));
+
+/*
+passport.use('facebookToken', new FacebookTokenStrategy({
+  clientID: config.oauth.facebook.clientID,
+  clientSecret: config.oauth.facebook.clientSecret
+}, async (accessToken, refreshToken, profile, done) => {
+  try {
+    console.log('profile', profile);
+    console.log('accessToken', accessToken);
+    console.log('refreshToken', refreshToken);
+
+    const DB = new Database(DBconfig);
+    //place holder
+    const existingUser = await DB.query(UserModel.GetUser(), profile.emails[0].value);
+    if (existingUser.length !== 0){
+      if (existingUser.authType === 2 && exstingUser.authField === profile.id){
+        done(null, existingUser);
+      }
+      else{
+        throw 'Email already exists';
+      }
+    }
+    //const newUser = await DB.query(UserModel.InsertUser(), )
+
+  } catch(error) {
+    console.log(error);
+    done(error, false, error.message);
+  }
+}));
+*/
